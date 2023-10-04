53b597445950f8bf6d37bc1123550e035af951e7
Delacruz-K <kondelacruz666@gmail.com>
2023-10-04 02:16:12 +0000

    Commit worksheet 3 (wip)

---
+++ worksheet#3/RWorksheet_DelaCruz#3a.R
@@ -0,0 +1,28 @@
+#1.
+Uppercase <- LETTERS
+Uppercase
+
+Lowercase <- letters
+Lowercase
+
+#1.a
+
+eleven <- Uppercase[1:11]
+eleven
+
+#1.b
+
+Uppercase[]
+
+#1.c
+
+
+#1.d
+
+five_last <- tail(Lowercase,5)
+five_last
+
+#1.e
+
+fifteen_niyon <- Lowercase [15:24]
+fifteen_niyon
